a317fd7a25db9f20a88ea103d3c79e97577a4480
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-01-14 01:08:01 +0530

    candidate-testing | Part-1 | Completed

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,7 +2,7 @@ const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
-// TODO 1.1a: Define candidateName // 
+/* TODO 1.1a: Define candidateName */
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
